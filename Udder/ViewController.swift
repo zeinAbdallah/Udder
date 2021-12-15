229feeba245748046c51c57bea4c5222f763e1f3
zeinAbdallah <93872238+zeinAbdallah@users.noreply.github.com>
2021-12-15 18:20:36 +0200

    add will disappear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func viewWillDisappear(_ animated: Bool) {
+        //
+    }
 
 }
 
